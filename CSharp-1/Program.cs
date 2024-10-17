ca10ad0edf7941248640f2e949f73fe6ba21e760
Vitor Matheus Almeida <matheus_famas@hotmail.com>
2024-10-17 16:15:39 -0300

    PullRequest

--- CSharp-1/Program.cs
+++ CSharp-1/Program.cs
@@ -9,6 +9,7 @@ namespace CSharp_1
             Console.WriteLine("Hello World!, esse esta sendo o primeiro codigo dentro do Rider e realizando o ritual" +
                               "do hello world para começar a linguegem C#");
             Console.WriteLine("isso é um programa de teste para testar os diretorios dentro do rider");
+            Console.WriteLine("pullRequest");
         }
     }
 }

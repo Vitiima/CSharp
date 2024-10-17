9f46d862beebea72021c381b3f458967d7c7d701
Vitor Matheus Almeida <matheus_famas@hotmail.com>
2024-10-17 16:10:45 -0300

    Primeiro commit dento do Rider

---
+++ CSharp-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, esse esta sendo o primeiro codigo dentro do Rider e realizando o ritual" +
+                              "do hello world para começar a linguegem C#");
+            Console.WriteLine("isso é um programa de teste para testar os diretorios dentro do rider");
+        }
+    }
+}
